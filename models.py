085a27f8e901b7b044e47cf485b8c4018e49bcd2
Dharian Malchan <dharianmalchan@gmail.com>
2020-05-27 01:19:11 +0000

    Edited models and index

--- models.py
+++ models.py
@@ -2,7 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 import datetime
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True ,nullable=False)
+    email = db.Column(db.String(80), unique=True ,nullable=False)
+    password = db.Column(db.String(50), unique=True ,nullable=False)
 
+    def toDict(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'password': self.password
+
+        }
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), unique=True ,nullable=False)
+
+    def toDict(self):
+        pass
+       
+class UserReact(db.Model):
+    pass
+    
 class Logs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     studentId =  db.Column(db.Integer, nullable=False)
